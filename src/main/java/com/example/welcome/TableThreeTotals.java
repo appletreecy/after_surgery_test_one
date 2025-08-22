46bb2f4d9da1ed46a9c6d3c4d67279608ec0493b
appletreecy <appletreecy@gmail.com>
2025-08-22 22:04:45 +1000

    paging completed for tableThree

---
+++ src/main/java/com/example/welcome/TableThreeTotals.java
@@ -0,0 +1,11 @@
+package com.example.welcome;
+
+public record TableThreeTotals(
+    Long totalNumOfJointComplicationCount,
+    Long totalNumOfMotorDysfunctionCount,
+    Long totalNumOfTraumaComplicationCount,
+    Long totalNumOfAnkleComplicationCount,
+    Long totalNumOfPediatricAdverseEventCount,
+    Long totalNumOfSpinalComplicationCount,
+    Long totalNumOfHandSurgeryComplicationCount
+) {}
