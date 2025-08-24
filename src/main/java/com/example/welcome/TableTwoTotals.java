c594cf6c1d170dcc8ed96fd6e1b38bdbed31410a
appletreecy <appletreecy@gmail.com>
2025-08-24 20:43:11 +1000

    paging for tableTwo completed

---
+++ src/main/java/com/example/welcome/TableTwoTotals.java
@@ -0,0 +1,19 @@
+package com.example.welcome;
+
+public record TableTwoTotals(
+        Long totalNumOfNauseaAndVomiting,
+        Long totalNumOfDizziness,
+        Long totalNumOfNauseaAndVomitingAndDizziness,
+        Long totalNumOfItching,
+        Long totalNumOfAllergicRash,
+        Long totalNumOfProlongedAnestheticRecovery,
+        Long totalNumOfPunctureSiteAbnormality,
+        Long totalNumOfAbdominalDistension,
+        Long totalNumOfEndotrachealIntubationDiscomfort,
+        Long totalNumOfEpigastricPain,
+        Long totalNumOfDelirium,
+        Long totalNumOfChestDiscomfort,
+        Long totalNumOfTourniquetReactionnumOfTourniquetReaction,
+        Long totalNumOfOther
+) {}
+
--- src/main/java/com/example/welcome/repository/AfterSurgeryTableTwoRepository.java
+++ src/main/java/com/example/welcome/repository/AfterSurgeryTableTwoRepository.java
@@ -1,11 +1,44 @@
 package com.example.welcome.repository;
 
+import com.example.welcome.TableThreeTotals;
+import com.example.welcome.TableTwoTotals;
+import com.example.welcome.model.AfterSurgeryTableThree;
 import com.example.welcome.model.AfterSurgeryTableTwo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 @Repository
 public interface AfterSurgeryTableTwoRepository extends JpaRepository<AfterSurgeryTableTwo, Long> {
+    // for TableTwo
+    Page<AfterSurgeryTableTwo> findByDateBetween(LocalDate start, LocalDate end, Pageable pageable);
+
+    @Query("""
+      select new com.example.welcome.TableTwoTotals(
+        coalesce(sum(t.numOfNauseaAndVomiting), 0L),
+        coalesce(sum(t.numOfDizziness), 0L),
+        coalesce(sum(t.numOfNauseaAndVomitingAndDizziness), 0L),
+        coalesce(sum(t.numOfItching), 0L),
+        coalesce(sum(t.numOfAllergicRash), 0L),
+        coalesce(sum(t.numOfProlongedAnestheticRecovery), 0L),
+        coalesce(sum(t.numOfPunctureSiteAbnormality), 0L),
+        coalesce(sum(t.numOfAbdominalDistension), 0L),
+        coalesce(sum(t.numOfEndotrachealIntubationDiscomfort), 0L),
+        coalesce(sum(t.numOfEpigastricPain), 0L),
+        coalesce(sum(t.numOfDelirium), 0L),
+        coalesce(sum(t.numOfChestDiscomfort), 0L),
+        coalesce(sum(t.numOfTourniquetReaction), 0L),
+        coalesce(sum(t.numOfOther), 0L)
+      )
+      from AfterSurgeryTableTwo t
+      where t.date between :start and :end
+    """)
+    TableTwoTotals computeTotalsInRange(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
 
 
